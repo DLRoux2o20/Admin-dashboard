46a23e65c056fb31073806b138a5f10ccf85dd2f
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2025-12-03 14:46:49 +0200

    Make font sizes a bit more responsive for large laptop screens and add JS file which changes star icon on click.

---
+++ main.js
@@ -0,0 +1,9 @@
+let stars = document.querySelectorAll(".fa-star");
+
+stars.forEach((element) => {
+	element.addEventListener("click", (event) => {
+        console.log(event.target);
+		event.target.classList.toggle("fa-solid");
+		event.target.classList.toggle("fa-regular");
+	});
+});
