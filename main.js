b6f034c16b1ae62d7aa96f95f9426c1c61feec06
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2025-12-03 14:48:22 +0200

    Changed nothing.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let stars = document.querySelectorAll(".fa-star");
 
 stars.forEach((element) => {
 	element.addEventListener("click", (event) => {
-        console.log(event.target);
+		console.log(event.target);
 		event.target.classList.toggle("fa-solid");
 		event.target.classList.toggle("fa-regular");
 	});
